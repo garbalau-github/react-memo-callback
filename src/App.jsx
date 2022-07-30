ffe466b057584b702a969f6957ed66dfce6fd720
Nick Garbalau <garbalaunick@protonmail.com>
2022-07-30 15:57:41 +0300

    useMemo, useCallback preset created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import Main from './components/Main';
+import { useState, useMemo, useCallback } from 'react';
 import './App.css';
 
 const App = () => {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+
+  const user = { name, surname };
+
   return (
     <div className='App'>
-      <Main />
+      <h1>useMemo, useCallback</h1>
+      <br />
+      <form>
+        <div>
+          <label>
+            <div>Name</div>
+            <input
+              type='text'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            <div>Surname</div>
+            <input
+              type='text'
+              value={surname}
+              onChange={(e) => setSurname(e.target.value)}
+            />
+          </label>
+        </div>
+      </form>
+      <br />
+      <pre>{JSON.stringify(user, null, 2)}</pre>
     </div>
   );
 };
